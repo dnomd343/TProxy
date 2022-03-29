6d0e031dedd547688b34782e2c4a18d5a73fc844
dnomd343 <dnomd343@gmail.com>
2022-03-29 23:41:49 +0800

    update: rule update script

--- load.sh
+++ load.sh
@@ -158,10 +158,9 @@ EOF
 
 load_update_script() {
   cat > $ASSET_DIR/update.sh << "EOF"
-VERSION=$(curl -sL "https://api.github.com/repos/Loyalsoldier/v2ray-rules-dat/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 mkdir temp/ && cd temp/
-wget "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/download/$VERSION/geoip.dat"
-wget "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/download/$VERSION/geosite.dat"
+wget "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
+wget "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
 [ -s "geoip.dat" ] && mv -f geoip.dat ../
 [ -s "geosite.dat" ] && mv -f geosite.dat ../
 cd ../ && rm -rf temp/
